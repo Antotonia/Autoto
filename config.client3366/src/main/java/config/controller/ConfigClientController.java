616e87100db45b1d9b9becd91831028c844307f8
Autotonia <1720304495@qq.com>
2021-01-15 16:46:08 +0800

    删除文件

---
+++ config.client3366/src/main/java/config/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "client")
+@RefreshScope //刷新
+public class ConfigClientController {
+    @Value("${spring.profiles.active}")
+    private String active;
+
+    @GetMapping(value = "/getActive")
+    public String getActive(){
+        return active;
+    }
+}
